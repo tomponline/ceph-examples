57419e8b0e80ddbe5be50a33eb9fdb8894d9a017
Thomas Parrott <tomp@tomp.uk>
2022-07-14 15:32:42 +0100

    minio
    Signed-off-by: Thomas Parrott <tomp@tomp.uk>

--- minios3client/main.go
+++ minios3client/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-var endpoint string = "10.96.75.11:8080"
+var endpoint string = "127.0.0.1:9000"
 
 func client(accessKey string, accessSecret string) *minio.Client {
 	// Initialize minio client object.
@@ -23,5 +25,6 @@ func client(accessKey string, accessSecret string) *minio.Client {
 }
 
 func main() {
-
+	adminUser := client("L4G8GAN5X1EYNI8VJ6YV", "Ti8gFflHZALYN5iFrPmzO7g6oGUVjqUNDJnIMbOM")
+	fmt.Println(adminUser.ListBuckets(context.Background()))
 }
