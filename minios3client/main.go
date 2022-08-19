4dd653e77527196b679f5147a7be10c046efd11c
Thomas Parrott <tomp@tomp.uk>
2022-08-19 15:35:30 +0100

    minio transport options
    Signed-off-by: Thomas Parrott <tomp@tomp.uk>

--- minios3client/main.go
+++ minios3client/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -14,8 +16,9 @@ var endpoint string = "127.0.0.1:9000"
 func client(accessKey string, accessSecret string) *minio.Client {
 	// Initialize minio client object.
 	minioClient, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKey, accessSecret, ""),
-		Secure: false,
+		Creds:     credentials.NewStaticV4(accessKey, accessSecret, ""),
+		Secure:    true,
+		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 	})
 	if err != nil {
 		log.Fatalln(err)
@@ -25,6 +28,15 @@ func client(accessKey string, accessSecret string) *minio.Client {
 }
 
 func main() {
-	adminUser := client("L4G8GAN5X1EYNI8VJ6YV", "Ti8gFflHZALYN5iFrPmzO7g6oGUVjqUNDJnIMbOM")
+	adminUser := client("MZc88riW5mXayhlO", "xDjKf9C4VkoTuSMu4Fss5D0DELC37X2W")
 	fmt.Println(adminUser.ListBuckets(context.Background()))
+
+	objectCh := adminUser.ListObjects(context.Background(), "foo", minio.ListObjectsOptions{})
+	for object := range objectCh {
+		if object.Err != nil {
+			fmt.Println(object.Err)
+			return
+		}
+		fmt.Println(object)
+	}
 }
